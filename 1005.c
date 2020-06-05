b7ff6244445ac153722ad74875606cbe6f10d0a2
kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-05 00:50:28 -0300

    1005 - Media 1
    1005 URI

---
+++ 1005.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){
+   double a,b,media;
+   scanf("%lf", &a);
+   scanf("%lf", &b);
+   media = ((3.5*a) + (7.5*b)) / 11;
+   printf("MEDIA = %.5lf\n", media);
+system("pause");}
