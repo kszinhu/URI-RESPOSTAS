cfbdc3e0c8745ef8514f2d7eb8e0f56f2d5eff6e
Cassiano Henrique Ap. Rodrigues <66069923+kszinhu@users.noreply.github.com>
2020-06-05 20:05:37 -0300

    1114 - Senha Fixa
    1114 URI

---
+++ 1114.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main(){
+int senha;
+    scanf("%d",&senha);
+    while(senha!=2002){
+        printf("Senha Invalida\n");
+        scanf("%d",&senha);}
+    printf("Acesso Permitido\n");
+    system("pause");}
