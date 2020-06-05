70e044f3eb26c7e89d560a171f8d3d5f4c6c5a44
kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-05 11:01:09 -0300

    1041 - Coordenadas de um Ponto
    1041 URI

---
+++ 1041.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+ float x,y; 
+scanf("%f",&x);scanf("%f",&y);
+  if (x>0){ 
+  		if(y>0){ printf("Q1\n");}
+		if(y<0){ printf("Q4\n");} 
+  		if(y==0){ printf("Eixo X\n");}
+		}
+  if (x<0){
+   		if(y>0){ printf("Q2\n");} 
+  		if(y<0){ printf("Q3\n");} 
+  		if(y==0){ printf("Eixo X\n"); }}
+  if (x==0){
+    	if(y>0){printf("Eixo Y\n");}
+    	if(y<0){printf("Eixo Y\n");}
+    	if(y==0){printf("Origem\n");}}
+system("pause");}
