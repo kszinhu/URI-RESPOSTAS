9e7e0bebce04bf09e1d05476f4fbdc2ab202df30
Cassiano Henrique Ap. Rodrigues <66069923+kszinhu@users.noreply.github.com>
2020-06-06 16:24:06 -0300

    1146 - Sequências Crescentes
    1146 URI

---
+++ 1146.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(){
+    int x, y, i;
+    do{
+        scanf("%d", &x);
+        for (i=1;i<=x;i++){
+            if(i==x){
+                printf("%d\n",i);}
+            else{
+                printf("%d ", i);}}
+    }while(x!=0);
+system("pause");}
