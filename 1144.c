4fda94f7306ecf0fb42a17e00d4e6cd849f639a5
Cassiano Henrique Ap. Rodrigues <66069923+kszinhu@users.noreply.github.com>
2020-06-06 16:17:17 -0300

    1144 - Sequência Lógica
    1144 URI

---
+++ 1144.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){
+int n, i;
+    scanf("%d", &n);
+    for (i=1;i<=n;i++){
+        printf("%d %d %d\n", i,i*i,i*i*i);
+        printf("%d %d %d\n", i,((i*i)+1),((i*i*i)+1));}
+    system("pause");}
