a36d7f00a9f4789b9c000e4aa60a922c15aa11f9
Cassiano Henrique Ap. Rodrigues <66069923+kszinhu@users.noreply.github.com>
2020-06-06 16:48:49 -0300

    2748 - Saída 2
    2748 URI

---
+++ 2748.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+    printf("---------------------------------------\n");
+    printf("|        Roberto                      |\n");
+    printf("|                                     |\n");
+    printf("|        5786                         |\n");
+    printf("|                                     |\n");
+    printf("|        UNIFEI                       |\n");
+    printf("---------------------------------------\n");
+    system("pause");}
