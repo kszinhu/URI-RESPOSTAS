b99221f5b6b63ed1cb2d76af1f3cf2cdf0b80d43
Kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-09 17:04:51 -0300

    1187 - Área Superior
    1187 URI

---
+++ 1187.c
@@ -0,0 +1,30 @@
+/*
+1187 - Área Superior
+> Cassiano Rodrigues
+> 09/06/2020
+*/
+#include <stdio.h>
+#define LINHA 12
+#define COLUNA 12
+main(){
+    int contador=0,i,j;
+    char O;
+    double soma=0; 
+    double matriz[LINHA][COLUNA];
+        scanf(" %c",&O);
+    for (i=0;i<LINHA;i++){
+        for (j=0;j<COLUNA;j++){
+            scanf("%lf",&matriz[i][j]);
+            if (j>i && (j+i)<=10){
+                soma+=matriz[i][j];
+                contador++;
+            }
+        }
+    }
+    if (O=='S'){
+        printf("%.1lf\n",soma);
+    }
+    else if (O=='M'){
+        printf("%.1lf\n",(soma/contador));
+    }        
+system("pause");}
