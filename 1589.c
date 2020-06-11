f2e00c0142ddb83ce91ada0973822bd579ea9846
Kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-11 16:08:55 -0300

    1589 - Bob Conduite
    1589 URI

---
+++ 1589.c
@@ -0,0 +1,14 @@
+/*
+1589 - Bob Conduite
+> Cassiano Rodrigues
+> 11/06/2020
+*/
+#include <stdio.h>
+main(){
+    int i=0,t,raio1,raio2;
+    scanf("%d",&t);
+    for (i=0;i<t;i++){
+        scanf("%d",&raio1);scanf("%d",&raio2);
+        printf("%d\n",(raio1+raio2));
+    }
+system("pause");}
