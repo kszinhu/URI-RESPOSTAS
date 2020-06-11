19fb5bba22a7859ae49d5324d94f3799faede040
Kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-11 16:08:43 -0300

    1564 - Vai Ter Copa?
    1564 URI

---
+++ 1564.c
@@ -0,0 +1,17 @@
+/*
+1564 - Vai Ter Copa?
+> Cassiano Rodrigues
+> 11/06/2020
+*/
+#include <stdio.h>
+main(){
+    int n;
+    while(scanf("%d",&n)!=EOF){
+        if (n>0){
+            printf("vai ter duas!\n");
+        }
+        else{
+            printf("vai ter copa!\n");
+        }        
+    }
+system("pause");}
