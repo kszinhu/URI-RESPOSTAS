e3810a201e5641051bca7fd6786c49993e9266c5
Kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-13 02:55:26 -0300

    1864 - Nossos Dias Nunca Voltarão
    1864 URI

---
+++ 1864.c
@@ -0,0 +1,15 @@
+/*
+1864 - Nossos Dias Nunca Voltarão
+> Cassiano Rodrigues
+> 12/06/2020
+*/
+#include <stdio.h>
+main(){
+    char frase[34]={'L','I','F','E',' ','I','S',' ','N','O','T',' ','A',' ','P','R','O','B','L','E','M',' ','T','O',' ','B','E',' ','S','O','L','V','E','D'};
+    int i,quantidade;
+    scanf("%d",&quantidade);
+    for (i=0;i<quantidade;i++){
+        printf("%c",frase[i]);
+    }
+    printf("\n");
+system("pause");}
