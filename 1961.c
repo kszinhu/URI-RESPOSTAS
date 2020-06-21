2e46311be632e9e9aea01e45a38085df3579e27d
Cassiano Rodrigues <rodrigues.cassiano2007@gmail.com>
2020-06-21 19:43:48 -0300

    1961 - Pula Sapo
    1961 URI

--- 1961.c
+++ 1961.c
@@ -6,12 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 main(){
-int pulo_sapo,num_canos,num_canost,tam_cano,anterior,sim_p=0,i; 
+    int pulo_sapo,num_canos,num_canost,tam_cano,anterior,sim_p=0,i; 
 
-	scanf("%d",&pulo_sapo);scanf("%d",&num_canos);
+		scanf("%d",&pulo_sapo);scanf("%d",&num_canos);
 	num_canost=num_canos;
 	num_canos--;
-	scanf("%d",&tam_cano);
+		scanf("%d",&tam_cano);
 	anterior=tam_cano;
 
 	for(i=0;i<num_canost;i++){
