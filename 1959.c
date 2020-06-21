c6136f13867352b93425bc89bbe4fdfb13b441b8
Cassiano Rodrigues <rodrigues.cassiano2007@gmail.com>
2020-06-21 19:42:07 -0300

    1959 - Polígonos Regulares Simples
    1959 URI

---
+++ 1959.c
@@ -0,0 +1,10 @@
+/*
+1959 - Polígonos Regulares Simples
+> Cassiano Rodrigues
+> 21/06/2020
+*/
+main(){
+	long long int n, l;
+	scanf("%lld",&n);scanf("%lld",&l);
+    printf("%lld\n",(n*l));
+system("pause");}
