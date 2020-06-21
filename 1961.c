fcf066ef48d23321d5596a5b3a07780e447c3209
Cassiano Rodrigues <rodrigues.cassiano2007@gmail.com>
2020-06-21 19:43:08 -0300

    1961 - Pula Sapo
    1961 URI

---
+++ 1961.c
@@ -0,0 +1,34 @@
+/*
+1961 - Pula Sapo
+> Cassiano Rodrigues
+> 21/06/2020
+*/
+#include <stdio.h>
+#include <stdlib.h>
+main(){
+int pulo_sapo,num_canos,num_canost,tam_cano,anterior,sim_p=0,i; 
+
+	scanf("%d",&pulo_sapo);scanf("%d",&num_canos);
+	num_canost=num_canos;
+	num_canos--;
+	scanf("%d",&tam_cano);
+	anterior=tam_cano;
+
+	for(i=0;i<num_canost;i++){
+		scanf("%d",&tam_cano);
+		
+		if(pulo_sapo<(abs(anterior-tam_cano))){
+			break;
+		}
+        else{
+			anterior=tam_cano;
+			sim_p++;
+		}
+	}
+	if(num_canost==sim_p){
+		printf("YOU WIN\n");
+	}
+    else{
+		printf("GAME OVER\n");
+	}	 
+system("pause");}
