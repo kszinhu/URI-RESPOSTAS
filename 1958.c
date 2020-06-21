192df37184506f528566c8b0abfe0f704fa2bc09
Cassiano Rodrigues <rodrigues.cassiano2007@gmail.com>
2020-06-21 19:41:49 -0300

    1958 - Notação Científica
    1958 URI

---
+++ 1958.c
@@ -0,0 +1,11 @@
+/*
+1958 - Notação Científica
+> Cassiano Rodrigues
+> 21/06/2020
+*/
+#include <stdio.h>
+main(){
+	long double num;
+    scanf("%LE",&num);
+    printf("%+.4LE\n",num);
+system("pause");}
