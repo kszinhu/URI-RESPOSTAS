bcb79091ac32af8ace2cd6768d1063f8b5d186b3
Cassiano Rodrigues <rodrigues.cassiano2007@gmail.com>
2020-06-20 23:52:31 -0300

    1957 - Converter para Hexadecimal
    1957 URI

---
+++ 1957.c
@@ -0,0 +1,13 @@
+/*
+1957 - Converter para Hexadecimal
+> Cassiano Rodrigues
+> 20/06/2020
+*/
+#include <stdio.h>
+#include <stdlib.h>
+main(){ 
+	unsigned long int num;
+    
+    scanf("%ld",&num);
+    printf("%lX\n",num); 
+system("pause");} 
