43694a4cb6eab8250ee1e15cd9a4912a4a56a103
Kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-22 22:07:47 -0300

    1963 - O Filme
    1963 URI

---
+++ 1963.c
@@ -0,0 +1,11 @@
+/*
+1963 - O Filme
+> Cassiano Rodrigues
+> 22/06/2020
+*/
+#include <stdio.h>
+main(){
+    double a,b;
+        scanf("%lf",&a);scanf("%lf",&b);
+    printf("%.2lf%%\n",(((b-a)/a)*100));
+system("pause");}
