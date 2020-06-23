ce6379e331ec0e4b291f7b4d5c8466837b841838
Kszinhu <rodrigues.cassiano2007@gmail.com>
2020-06-23 12:19:13 -0300

    2510 - Batmain
    2510 URI

---
+++ 2510.c
@@ -0,0 +1,16 @@
+/*
+2510 - Batmain
+> Cassiano Rodrigues
+> 23/06/2020
+*/
+#include <stdio.h>
+#include <stdlib.h>
+main(){
+    int t,i;
+    char n[30];
+        scanf("%d",&t);
+    for (i=0;i<t;i++){
+        scanf("%s",&n);
+        printf("Y\n");
+    }
+system("pause");}
